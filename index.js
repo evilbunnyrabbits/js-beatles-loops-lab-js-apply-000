081f721c1013dacc5c16a1cb2dd4d83e3ae7913d
beaming-subroutine-5164 <ben.garlock@gmail.com>
2020-05-11 14:04:10 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
 // add solution here
-const musicians = ['John Lennon', 'Paul McCartney', 'George Harrison', 'Ringo Starr'];
-const instruments = ['Guitar', 'Bass Guitar', 'Lead Guitar', 'Drums']
 
+array = []
 
 function theBeatlesPlay(musicians, instruments) {
-    array = []
+
     for (let counter = 0; counter < musicians.length; counter ++) {
         i = musicians[counter] + ' plays ' + instruments[counter];
         array.push(i);
@@ -13,4 +12,3 @@ function theBeatlesPlay(musicians, instruments) {
     return array
 }
 
-console.log(theBeatlesPlay(musicians, instruments))
