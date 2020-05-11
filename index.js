5b7adc8b48344131acfd092e12621c0addcf6efd
beaming-subroutine-5164 <ben.garlock@gmail.com>
2020-05-11 14:00:41 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 // add solution here
 
-let array = [];
-
 function theBeatlesPlay(musicians, instruments) {
-  
+    array = []
+    for (let counter = 0; counter < musicians.length; counter ++) {
+        i = musicians[counter] + ' plays ' + instruments[counter];
+        array.push(i);
+
+    }
+    return array
 }
